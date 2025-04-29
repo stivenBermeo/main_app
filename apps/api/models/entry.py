f17abf36bbee2606d1a62579ef1e62073a8916dd
stivenBermeo <bermeopalomares3@gmail.com>
2025-04-28 19:17:26 -0500

    chore: fix modifiable fields default value

--- apps/api/models/entry.py
+++ apps/api/models/entry.py
@@ -2,8 +2,8 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 
 class ModifiableEntryFields(BaseModel):
-  title: str | None = Field(description="The title", max_length=100)
-  body: str | None = Field(description="The content", max_length=590)
+  title: str | None = Field(description="The title", max_length=100, default=None)
+  body: str | None = Field(description="The content", max_length=590, default=None)
 
 class Entry(ModifiableEntryFields):
   id: int
