640bfbf624d0631910ef6a2902a756a99421a043
stivenBermeo <bermeopalomares3@gmail.com>
2025-05-07 17:47:12 -0500

    chore: create rough nav bar, remove react-query

--- apps/ui/src/main.tsx
+++ apps/ui/src/main.tsx
@@ -1,25 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
 import { ToastContainer } from 'react-toastify';
 
 import './index.css'
 import App from './App.tsx'
 
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
+      <div className='bg-primary'>
+         <a className="btn btn-primary w-50 text-center d-inline-block" href='/'>Home</a>
+         <a className="btn btn-primary w-50 text-center d-inline-block" href='/journal'>Journal</a>
+      </div>
         <Routes>
-          <Route path="/"  element={<App />}/>
+          <Route path="/"  element={<div></div>}/>
+          <Route path="/journal"  element={<App/>}/>
         </Routes>
-        </QueryClientProvider>
     </BrowserRouter>
     <ToastContainer/>
   </StrictMode>
